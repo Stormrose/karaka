7172990550180e73a285a075047c8067cbd7e23d
Emmanuel King Turner <eturnerx@gmail.com>
2022-01-15 20:28:58 +0000

    Reorder from chain->step to step->chain to prep for var importing and oracles

--- modules/mainTick.ts
+++ modules/mainTick.ts
@@ -15,33 +15,13 @@ export async function mainTick(config: KarakaConfig): Promise<boolean> {
         'mainTick() start.'
     )
 
-    if(config.hive) {
-        const hiveapiclient: HiveClient = new HiveClient(config.hive.apinode)
-
-        // Gather facts
-        let hivefacts: Facts = await HiveHelper.gatherFacts(config.hive.accounts, hiveapiclient)
-        hivefacts = FactHelper.mergeFacts(hivefacts, config.hive.constants)
-        // console.log('HIVE: Facts gathered, appplying rules.')
-
-        // Gather schedule of commands to execute
-        const hivecommandq: CommandForExecutions = getScheduleOfConsequentsToExecute(hivefacts, config.hive.rules, 'HIVE', true)
-        if(hivecommandq.length === 0) quietconsole.log('hivenorulesmatched', 'HIVE: No rules matched.')
-        // console.log(hivecommandq)
-
-        // Execute the schedule
-        for(const cmd of hivecommandq) {
-            orderid++
-            await HiveHelper.executeCommand(cmd, orderid, config.hive.accounts, hiveapiclient, true)
-            r = r || cmd.command.hassideeffects
-        }
-        // Report
-    }
-    // { HiveHelper; orderid; }
+    // Create connections
+    let hiveapiclient: HiveClient = new HiveClient(config.hive?.apinode ?? "https://api.hive.blog")
+    let hehiveapiclient: HiveClient = hiveapiclient
 
     if(config.hiveengine) {
-        // Use api-node from Hive, if specifics aren't declared for hive-engine
         config.hiveengine.apinode = config.hiveengine.apinode ?? config.hive?.apinode ?? "https://api.hive.blog"
-        const hiveapiclient: HiveClient = new HiveClient(config.hiveengine.apinode)
+        hehiveapiclient = new HiveClient(config.hiveengine.apinode)
 
         // Check if accounts
         for(const accountname of Object.keys(config.hiveengine.accounts)) {
@@ -51,30 +31,53 @@ export async function mainTick(config: KarakaConfig): Promise<boolean> {
                 config.hiveengine.accounts[accountname] = config[account.from].accounts[accountname]
             }
         }
+    }
 
-        // Gather facts
-        let hiveenginefacts: Facts = await HiveEngineHelper.gatherFacts(config.hiveengine.accounts, hiveapiclient, config.hiveengine.sidechainuri)
+    // Gather facts
+    let hivefacts: Facts = {}
+    let hiveenginefacts: Facts = {}
+
+    if(config.hive) {
+        hivefacts = await HiveHelper.gatherFacts(config.hive.accounts, hiveapiclient)
+        hivefacts = FactHelper.mergeFacts(hivefacts, config.hive.constants)
+    }
+    if(config.hiveengine) {
+        hiveenginefacts = await HiveEngineHelper.gatherFacts(config.hiveengine.accounts, hehiveapiclient, config.hiveengine.sidechainuri)
         hiveenginefacts = FactHelper.mergeFacts(hiveenginefacts, config.hiveengine.constants)
-        // console.log(hiveenginefacts)
-        // console.log('HIVEENGINE: Facts gathered, appplying rules.')
+    }
 
-        // Gather schedule of commands to execute
-        const hiveenginecommandq: CommandForExecutions = getScheduleOfConsequentsToExecute(hiveenginefacts, config.hiveengine.rules, 'HIVEENGINE', true)
+    // Gather schedule of commands to execute
+    let hivecommandq: CommandForExecutions = []
+    let hiveenginecommandq: CommandForExecutions = []
+
+    if(config.hive) {
+        hivecommandq = getScheduleOfConsequentsToExecute(hivefacts, config.hive.rules, 'HIVE', true)
+        if(hivecommandq.length === 0) quietconsole.log('hivenorulesmatched', 'HIVE: No rules matched.')        
+    }
+    if(config.hiveengine) {
+        hiveenginecommandq = getScheduleOfConsequentsToExecute(hiveenginefacts, config.hiveengine.rules, 'HIVEENGINE', true)
         if(hiveenginecommandq.length === 0) quietconsole.log('hiveenginenorulesmatched', 'HIVEENGINE: No rules matched.')
-        // console.log(hiveenginecommandq)
+    }
 
-        // Execute the schedule
+    // Execute the schedule
+    if(config.hive) {
+        for(const cmd of hivecommandq) {
+            orderid++
+            await HiveHelper.executeCommand(cmd, orderid, config.hive.accounts, hiveapiclient, true)
+            r = r || cmd.command.hassideeffects
+        }        
+    }
+    if(config.hiveengine) {
         for(const cmd of hiveenginecommandq) {
-            await HiveEngineHelper.executeCommand(cmd, 0, config.hiveengine.accounts, hiveapiclient, true)
+            await HiveEngineHelper.executeCommand(cmd, 0, config.hiveengine.accounts, hehiveapiclient, true)
             r = r || cmd.command.hassideeffects
         }
-        // Report
-
-        quietconsole.log(
-            'tickend',
-            'mainTick() end.'
-        )
     }
+
+    quietconsole.log(
+        'tickend',
+        'mainTick() end.'
+    )
     return Promise.resolve(r)
 }
 
